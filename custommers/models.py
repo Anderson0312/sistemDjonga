c31872bde9ba29d33d922e2a35f029096def8eef
AndersonMoura <andersonmoura812@gmail.com>
2024-04-30 15:50:36 -0300

    att

--- custommers/models.py
+++ custommers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class custommer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    profissao = models.CharField(max_length=100, null=True)
+    sexo = models.CharField(max_length=10, null=True)
+    address = models.CharField(max_length=50, null=True)
+    city = models.CharField(max_length=50, null=True)
+    cep = models.CharField(max_length=50, null=True)
