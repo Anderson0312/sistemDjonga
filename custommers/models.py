0b49372f7d8dd78543a390da4d4c4cecfdc65578
AndersonMoura <andersonmoura812@gmail.com>
2024-05-02 16:10:15 -0300

    att

--- custommers/models.py
+++ custommers/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class custommer(models.Model):
+    email = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     profissao = models.CharField(max_length=100, null=True)
     sexo = models.CharField(max_length=10, null=True)
--- custommers/views.py
+++ custommers/views.py
@@ -4,13 +4,29 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from django.contrib import messages
+
+from custommers.models import custommer
+
 # Create your views here.
 def create_custommmer(request):
     if request.method == "GET":
             return render(request, 'clientes.html')         
     else:
-        print('Creating')
-        return redirect(reverse('client'))
-    # template = loader.get_template('clientes.html')
-    
-    # return HttpResponse(template.render(context, request))
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        profissao = request.POST.get('profissao')
+        sexo = request.POST.get('sexo')
+        address = request.POST.get('address')
+        city = request.POST.get('city')
+        cep = request.POST.get('cep')
+        
+        client = custommer.objects.filter(email=email).first()
+        
+        if client:
+            messages.error(request, 'Já existe um usuario com este cliente')
+        else:
+            client = custommer.objects.create(first_name=first_name, last_name=last_name, email=email, phone=phone, profissao=profissao, sexo=sexo, address=address, city=city, cep=cep)
+            return redirect(reverse('client'))
