7a3b91a4f9b61acdba85dd39a4737535dcb02a46
AndersonMoura <andersonmoura812@gmail.com>
2024-05-08 14:52:19 -0300

    att

--- SistemEmpresarial/settings.py
+++ SistemEmpresarial/settings.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
+    # 'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
